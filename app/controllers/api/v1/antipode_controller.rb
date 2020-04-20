cefc582ef421a231a2dd482f0386f75ef881fb20
William Meighan <williamdmeighan@gmail.com>
2020-04-20 11:16:04 -0600

    take params as argument

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::AntipodeController < ApplicationController
-
   def show
-    render json: AntipodeSerializer.new(AntipodeFacade.new(params[:location])
+    render json: AntipodeSerializer.new(AntipodeFacade.new(params))
   end
-
 end
