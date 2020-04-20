33d946a092db53d7a1c859be54554d545b76559e
William Meighan <williamdmeighan@gmail.com>
2020-04-20 10:25:05 -0600

    tentative service

---
+++ app/services/antipode_service.rb
@@ -0,0 +1,28 @@
+class AmypodeService
+
+  def antipode(location)
+    json = get_json(request_antipode(location))
+    json[:data][:attributes]
+  end
+
+  def request_antipode(location)
+    conn.get("antipodes") do |req|
+      req.params[:lat] = location.latitude
+      req.params[:long] = location.longitude
+    end
+  end
+
+  private
+
+  def conn
+    Faraday.new("http://amypode.herokuapp.com/api/v1/") do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.headers[:api_key] = ENV['WILL_API']
+    end
+  end
+
+  def get_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
