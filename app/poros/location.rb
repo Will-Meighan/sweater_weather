efe82b45bb77efb037e76ac4ec659d0e9fbd8e15
William Meighan <williamdmeighan@gmail.com>
2020-04-20 11:54:21 -0600

    updae parameters for spelling

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -1,10 +1,10 @@
 
 class Location
-  attr_reader :name, :lat, :lon
+  attr_reader :name, :lat, :long
 
   def initialize(data)
     @name = data[:results][0][:formatted_address]
     @lat = data[:results][0][:geometry][:location][:lat]
-    @lon = data[:results][0][:geometry][:location][:lng]
+    @long = data[:results][0][:geometry][:location][:lng]
   end
 end
