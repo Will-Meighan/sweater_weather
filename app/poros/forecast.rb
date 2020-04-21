96320997ac45a07ac95f237097b37fbe83388cae
William Meighan <williamdmeighan@gmail.com>
2020-04-21 11:05:43 -0600

    add helper method to forecast

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -4,4 +4,18 @@ class Forecast
   def initialize(data)
     @data = data
   end
+
+  def arrival_forecast(duration)
+
+    current_time = @data[:current][:dt]
+    arrival_time = current_time + duration
+
+    forecast = @data[:hourly].min_by do |time|
+      (arrival_time - time[:dt]).abs
+    end
+
+    weather = forecast[:weather][0][:description]
+    temp = forecast[:temp]
+    {temp: temp, description: weather}
+  end
 end
