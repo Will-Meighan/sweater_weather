629b570fbc7a1e1defb3eda493b955f5ee613eba
William Meighan <williamdmeighan@gmail.com>
2020-04-21 11:06:16 -0600

    updare forecast facade with additional initializers

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,9 +1,11 @@
 class ForecastFacade
-  attr_reader :id, :name, :data
+  attr_reader :id, :name, :data, :arrival_forecast
 
   def initialize(location)
     @id = nil
-    @data = ForecastService.new(GeolocationFacade.new(location).coordinates).forecast.data
+    location = GeolocationFacade.new(location)
+    @data = ForecastService.new(location.lat, location.long).forecast.data
+    @arrival_forecast = ForecastService.new(location.lat, location.long).forecast
   end
 
   def current
@@ -17,4 +19,5 @@ class ForecastFacade
   def daily
     @data[:daily]
   end
+
 end
