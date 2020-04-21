7ac348e98d23ff24220f74b695ae37d3ed9244ef
William Meighan <williamdmeighan@gmail.com>
2020-04-21 11:24:32 -0600

    refactor parse

--- app/services/background_service.rb
+++ app/services/background_service.rb
@@ -4,16 +4,17 @@ class BackgroundService
   end
 
   def url
+    response = get
     JSON.parse(response.body, symbolize_names: true)[:results][0][:urls][:raw]
   end
 
   def conn
-    Faraday.new('https://api.unsplash.com/')
+    just_city = @city.split(',').first
+    Faraday.new("https://api.unsplash.com/search/photos?query=#{just_city}&client_id=#{ENV['UNSPLASH_ACCESS_KEY']}")
   end
 
-  def response
-    just_city = @city.split(',').first
-    conn.get("search/photos?query=#{just_city}&client_id=#{ENV['UNSPLASH_ACCESS_KEY']}")
+  def get
+    conn.get
   end
 
 end
